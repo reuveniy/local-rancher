085c3df5b78fbcae60f12a2037e4f298eaddcc7f
Yoseph Reuveni <yoseph@jet.com>
2016-05-23 13:53:59 -0400

    fix set path to apply path globally

--- scripts/000_set_path.sh
+++ scripts/000_set_path.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eux
-if [[ "${PATH}" != *"${HOME}/bin"* ]]; then
-   echo 'export PATH=${PATH}:${HOME}/bin' | tee -a ~/.bashrc
-fi
 if [[ "${PATH}" != *"/vagrant/bin"* ]]; then
-   echo 'export PATH=${PATH}:/vagrant/bin' | tee -a ~/.bashrc
+   echo "export PATH=\"${PATH}:/vagrant/bin\"" | sudo tee /etc/environment
 fi
+
 
